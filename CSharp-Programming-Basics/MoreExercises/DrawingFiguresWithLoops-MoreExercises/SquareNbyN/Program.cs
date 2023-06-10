2b129074d51ea7b4c684b338959669f9b69bc1a4
Teodor Golisharski <t.golisharski@abv.bg>
2023-06-10 23:17:02 +0300

    Solutions to More Exercises Programming Basics added

---
+++ CSharp-Programming-Basics/MoreExercises/DrawingFiguresWithLoops-MoreExercises/SquareNbyN/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Rectangle10by10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.Write("\n");
+            }
+        }
+    }
+}
+
+
