9bcdfd2c2cb2766b069d05f6cd73413179fea8dd
Teodor Golisharski <t.golisharski@abv.bg>
2023-06-11 23:59:44 +0300

    Solutions to Fundamentals added

---
+++ CSharp-Fundamentals/Lab/LabBasicSyntax/MultiplicationTable/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MultiplicationTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            for (int i = num2; i <= 10; i++)
+            {
+                Console.WriteLine($"{num1} X {i} = {num1*i}");
+            }
+            if(num2>10)
+            {
+                Console.WriteLine($"{num1} X {num2} = {num1 * num2}");
+            }
+        }
+    }
+}
---
+++ CSharp-Fundamentals/Lab/LabBasicSyntax/StudentInformation/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace StudentInformation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            double avgGrade = double.Parse(Console.ReadLine());
+            Console.WriteLine($"Name: {name}, Age: {age}, Grade: {avgGrade:f2}");
+        }
+    }
+}
