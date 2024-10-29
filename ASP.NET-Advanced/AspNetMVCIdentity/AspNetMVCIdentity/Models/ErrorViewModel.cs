9fecb151cc4f1a1ad9c157a4dc62a9afe028bd83
Teodor Golisharski <t.golisharski@abv.bg>
2024-10-29 19:48:06 +0200

    ASP.NET-Advanced folder created
    ASP.NET-Advanced folder created and AspNetMVCIdentity coursework project is uploaded

---
+++ ASP.NET-Advanced/AspNetMVCIdentity/AspNetMVCIdentity/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AspNetMVCIdentity.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
